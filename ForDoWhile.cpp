0a11d229b40f3aa21d94f5a379f223e16c1c9a63
Cruelas <irula5198@gmail.com>
2024-03-19 14:47:10 +0700

    249_FoopingFungsi.cpp

---
+++ ForDoWhile.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+    int i;
+    string nama[5]
+    for (i = 7; >0 ; i++){
+            cout << i << "." << "Choirul Amir Siregar" << endl;
+    }
+    for (i= 0;i <5; i++) {
+        cout <<"Masukkan Nama =";
+        cin >> nama[i]
+    }
+
+        cout << endl;
+        cout << "=================";
+        cout << "KUMPULAN NAMA-NAMA" << endl;
+
+        for (i=0; i <5;i++){
+
+            cout << "Namanya Adalah" << nama [i] << endl;
+        }
+
+
+    
